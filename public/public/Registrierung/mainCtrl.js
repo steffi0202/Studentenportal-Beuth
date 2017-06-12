adc790c77267fcd17af057e116d5763f31162564
AlexRoessner1986 <alexander.roessner@web.de>
2017-06-12 20:24:12 +0200

    Anpassungen

---
+++ public/public/Registrierung/mainCtrl.js
@@ -0,0 +1,21 @@
+angular.module('mainApplication', ['fileModelDirective', 'uploadFileService'])
+
+.controller('mainCtrl', function($scope, uploadFile) {
+	$scope.file = {};
+	$scope.Submit = function(){
+		$scope.uploading = true;
+		uploadFile.upload($scope.file).then(function(data){
+			if(data.data.success){
+				$scope.uploading = false;
+				$scope.alert = 'alert alert-success';
+				$scope.message = data.data.message;
+				$scope.file = {};
+			} else{
+				$scope.uploading = false;
+				$scope.alert = 'alert alert-danger';
+				$scope.message = data.data.message;
+				$scope.file = {};
+			}
+		});
+	}
+});
